10af7f1fb7bbb241851505ba145ed9e8518128ca
Evan Klitzke <evan@eklitzke.org>
2014-11-11 16:59:41 -0800

    augment it

--- runstap.sh
+++ runstap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-OUT="example.txt"
-echo "going to run stap with output to $OUT"
-echo
-set -eux
-stap -DMAXACTION=10000 -DSTP_NO_OVERLOAD -I /usr/share/doc/systemtap-client/examples/general/tapset -v /usr/share/doc/systemtap-client/examples/general/py2example.stp -c "python timeout_example.py" > "$OUT"
+set -eu
 
-set +x
+run_with_echo() {
+  echo 1>&2 "$@"
+  echo 1>&2
+  "$@"
+}
+
+OUT="example.txt"
+if [ $# -eq 1 ]; then
+  OUT="$1"
+fi
+
+echo "going to run stap with output to: $OUT"
 echo
-echo "get stap output with:"
-echo "less $OUT"
+run_with_echo stap -DMAXACTION=10000 -DSTP_NO_OVERLOAD -I /usr/share/doc/systemtap-client/examples/general/tapset -v /usr/share/doc/systemtap-client/examples/general/py2example.stp -c "python timeout_example.py" > "$OUT"
